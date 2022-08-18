1f8e38ce82e3cc8b13a0a96002a3258eb05f3e0d
abrokwas <66515527+abrokwas@users.noreply.github.com>
2022-08-18 17:56:04 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, SafeAreaView, Platform, StatusBar} from 'react-native';
+import Constants from 'expo-constants';
+import {colors} from './src/utils/colors'
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello Sams World!</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? 50 : 0,
+    backgroundColor: colors.darkBlue
+  },
+
+  text: {
+    color: colors.white
+  }
+});
